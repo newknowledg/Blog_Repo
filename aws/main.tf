bb8afc79f25db9ccb1bdf9640d2ba746deb70ed7
joj <young2k15@tutanota.com>
2023-10-08 17:17:49 -0400

    update

--- aws/main.tf
+++ aws/main.tf
@@ -1,19 +1,11 @@
-resource "google_compute_instane" "wordpress" {
-    name = "wordpress_instance"
-    machine_type = "e2-micro"
-    zone = "us-central1-a"
-    allow_stopping_for_update = true
-
-    boot_disk{
-        initialize_params{
-            image = "debian-cloud/debian-11"
-        }
+resource "aws_db_instance" "wordpress" {
+    allocated_storage = 20
+    db_name = "wordpress_db"
+    engine = "postgres"
+    engine_version = "15.3"
+    instance_class = "db.t3.micro"
+    parameter_group_name = "default.postgresql_15.3"
+    skip_final_snapshot = true
+    username = "{__USER__}"
+    password = "{__PASS__}"
     }
-
-    network_interface {
-        network = default
-        access_config{
-            //necessary even empty
-        }
-    }
-}
