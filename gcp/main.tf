918b8de6455be8aeff936691cf0cc945cb367a98
joj <young2k15@tutanota.com>
2023-10-08 14:30:55 -0400

    update

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,4 +1,19 @@
-provider "google" {
-    project  = "feisty-proton-401321"
-    credentials = "${{ secrets.GCP_}}"
+resource "google_compute_instane" "wordpress" {
+    name = "wordpress_instance"
+    machine_type = "e2-micro"
+    zone = "us-central1-a"
+    allow_stopping_for_update = true
+
+    boot_disk{
+        initialize_params{
+            image = "debian-cloud/debian-11"
+        }
+    }
+
+    network_interface {
+        network = default
+        access_config{
+            //necessary even empty
+        }
+    }
 }
