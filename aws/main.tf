fb69c1a7afc01a8982f40c1a6cf9ce387b533fc8
joj <young2k15@tutanota.com>
2023-10-08 13:37:19 -0400

    update

--- aws/main.tf
+++ aws/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
     project = "feisty-proton-401321"
-    credentials = "${{ secrets.GCP_SA_KEY }}"
+    credentials = "${file("credentials.json")}"
     region = "us-central1"
     zone = "us-central1-c"
 }
