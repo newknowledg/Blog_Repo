90052aee2cdd38d62bb9f6a8e0e6ebd6a0089640
joj <young2k15@tutanota.com>
2023-10-08 13:25:06 -0400

    update

--- aws/main.tf
+++ aws/main.tf
@@ -4,3 +4,23 @@ provider "google" {
     region = "us-central1"
     zone = "us-central1-c"
 }
+
+resource "google_compute_instane" "wordpress" {
+    name = "wordpress_instance"
+    machine_type = "e2-micro"
+    zone = "us-central1-a"
+    allow_stopping_for_update = true
+
+    boot_disk{
+        initialize_params{
+            image = "debian-cloud/debian-11"
+        }
+    }
+
+    network_interface {
+        network = default
+        access_config{
+            //necessary even empty
+        }
+    }
+}
