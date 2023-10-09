b67b8e78fe7cf2f43602b2f5a57b0f83d9463d4b
joj <young2k15@tutanota.com>
2023-10-08 22:37:44 -0400

    update

--- aws/main.tf
+++ aws/main.tf
@@ -7,6 +7,10 @@ resource "aws_db_instance" "wordpress" {
     parameter_group_name = "default.postgres15"
     skip_final_snapshot = true
     identifier = "wordpress"
+    storage_encrypted = true
+    publicly_accessible = true
+    backup_retention_period = 7
+    delete_automated_backups = true
     username = "{__USER__}"
     password = "{__PASS__}"
     }
