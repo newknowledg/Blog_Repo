453116d1e090ed39cad7ccccfed88c4adcf76cae
joj <young2k15@tutanota.com>
2023-10-11 22:29:24 -0400

    last commit?

--- aws/main.tf
+++ aws/main.tf
@@ -1,10 +1,10 @@
 resource "aws_db_instance" "wordpress" {
     allocated_storage = 20
     db_name = "wordpress_db"
-    engine = "postgres"
-    engine_version = "15.3"
+    engine = "mysql"
+    engine_version = "5.7"
     instance_class = "db.t3.micro"
-    parameter_group_name = "default.postgres15"
+    parameter_group_name = "default.mysql5.7"
     skip_final_snapshot = true
     identifier = "wordpress"
     storage_encrypted = true
